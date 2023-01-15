fa97c547ac29a12ea67a1b44a9ba27b94dd84419
psycake <52770775+psycake@users.noreply.github.com>
2023-01-15 12:04:35 -0600

    Rename Lingkesto to Lingokesto

--- lingokesto/obs.html/static/obsidian_core.js
+++ lingokesto/obs.html/static/obsidian_core.js
@@ -10,7 +10,7 @@ var CONFIGURED_HTML_URL_PREFIX = "/lingokesto";
 var RELATIVE_PATHS = 0;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '193697177562139923509714343480319982612'                       // used to check whether the localStorage data is stale
+var gzip_hash = '234967896613367696412920836600157722884'                       // used to check whether the localStorage data is stale
 
 // global cache
 var fn_cache_ls_available = null;
