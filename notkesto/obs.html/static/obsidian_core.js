0173ac80d2992ea315f67d5c98df4d13350d0e29
psycake <52770775+psycake@users.noreply.github.com>
2023-02-06 19:59:20 -0600

    Small tag changes

--- notkesto/obs.html/static/obsidian_core.js
+++ notkesto/obs.html/static/obsidian_core.js
@@ -10,7 +10,7 @@ var CONFIGURED_HTML_URL_PREFIX = "/notkesto";
 var RELATIVE_PATHS = 0;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '235141742464606824367974413294754803899'                       // used to check whether the localStorage data is stale
+var gzip_hash = '17860773140794073135913944952807159259'                       // used to check whether the localStorage data is stale
 
 // global cache
 var fn_cache_ls_available = null;
