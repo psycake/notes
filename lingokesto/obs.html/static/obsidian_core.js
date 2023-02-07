cb914322281037c59df7eec7f77db334c889ffae
psycake <52770775+psycake@users.noreply.github.com>
2023-02-06 19:19:06 -0600

    Fix CSS

--- lingokesto/obs.html/static/obsidian_core.js
+++ lingokesto/obs.html/static/obsidian_core.js
@@ -10,7 +10,7 @@ var CONFIGURED_HTML_URL_PREFIX = "/lingokesto";
 var RELATIVE_PATHS = 0;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '281120283999018593829369543035230303708'                       // used to check whether the localStorage data is stale
+var gzip_hash = '181265847148795315632699265856356926860'                       // used to check whether the localStorage data is stale
 
 // global cache
 var fn_cache_ls_available = null;
--- notkesto/obs.html/static/obsidian_core.js
+++ notkesto/obs.html/static/obsidian_core.js
@@ -10,7 +10,7 @@ var CONFIGURED_HTML_URL_PREFIX = "/notkesto";
 var RELATIVE_PATHS = 0;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '122812989953459741609564569276346142867'                       // used to check whether the localStorage data is stale
+var gzip_hash = '235141742464606824367974413294754803899'                       // used to check whether the localStorage data is stale
 
 // global cache
 var fn_cache_ls_available = null;
