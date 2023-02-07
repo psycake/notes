56a78380988a068affcd27a3f31a2400df9089ae
psycake <52770775+psycake@users.noreply.github.com>
2023-02-06 18:55:25 -0600

    Add Blog and *kesto links and implement folder notes in Notkesto

--- lingokesto/obs.html/static/obsidian_core.js
+++ lingokesto/obs.html/static/obsidian_core.js
@@ -10,7 +10,7 @@ var CONFIGURED_HTML_URL_PREFIX = "/lingokesto";
 var RELATIVE_PATHS = 0;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '234967896613367696412920836600157722884'                       // used to check whether the localStorage data is stale
+var gzip_hash = '281120283999018593829369543035230303708'                       // used to check whether the localStorage data is stale
 
 // global cache
 var fn_cache_ls_available = null;
