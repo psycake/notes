f9d15c986b1a728947320d37bd13bc87faa522fd
psycake <52770775+psycake@users.noreply.github.com>
2023-02-23 20:10:31 -0600

    Update asset names and add graph theory notes

--- notkesto/obs.html/static/obsidian_core.js
+++ notkesto/obs.html/static/obsidian_core.js
@@ -10,7 +10,7 @@ var CONFIGURED_HTML_URL_PREFIX = "/notkesto";
 var RELATIVE_PATHS = 0;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '326664852860207465861127449967522108694'                       // used to check whether the localStorage data is stale
+var gzip_hash = '132852762074256757937465023046265081963'                       // used to check whether the localStorage data is stale
 
 // global cache
 var fn_cache_ls_available = null;
