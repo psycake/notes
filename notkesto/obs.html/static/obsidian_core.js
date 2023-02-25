540d4b9a98c891b535d30283f89b1b04543b1523
psycake <52770775+psycake@users.noreply.github.com>
2023-02-25 10:06:24 -0600

    Remove title from index note and collapse "About" callout

--- notkesto/obs.html/static/obsidian_core.js
+++ notkesto/obs.html/static/obsidian_core.js
@@ -10,7 +10,7 @@ var CONFIGURED_HTML_URL_PREFIX = "/notkesto";
 var RELATIVE_PATHS = 0;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '317041857507631231289228917827934741890'                       // used to check whether the localStorage data is stale
+var gzip_hash = '119384592862785287843875316376865888474'                       // used to check whether the localStorage data is stale
 
 // global cache
 var fn_cache_ls_available = null;
