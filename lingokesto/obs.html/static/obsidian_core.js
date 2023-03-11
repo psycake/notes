a35f472e997680fe73cae039f62dbbafc1578e4c
psycake <52770775+psycake@users.noreply.github.com>
2023-03-10 20:21:42 -0600

    Correct Jangul link

--- lingokesto/obs.html/static/obsidian_core.js
+++ lingokesto/obs.html/static/obsidian_core.js
@@ -10,7 +10,7 @@ var CONFIGURED_HTML_URL_PREFIX = "/lingokesto";
 var RELATIVE_PATHS = 0;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '79923143504979430301891472325309880817'                       // used to check whether the localStorage data is stale
+var gzip_hash = '88085326319965623721737624623517289936'                       // used to check whether the localStorage data is stale
 
 // global cache
 var fn_cache_ls_available = null;
