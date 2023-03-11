f012400bf98e0344acba8120c5d538d4c7325e39
psycake <52770775+psycake@users.noreply.github.com>
2023-03-10 20:20:17 -0600

    Add interpolation notes and polish probability notes

--- notkesto/obs.html/static/obsidian_core.js
+++ notkesto/obs.html/static/obsidian_core.js
@@ -10,7 +10,7 @@ var CONFIGURED_HTML_URL_PREFIX = "/notkesto";
 var RELATIVE_PATHS = 0;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '279408795967968760802848490722032302755'                       // used to check whether the localStorage data is stale
+var gzip_hash = '114096582885280587346398857247936356107'                       // used to check whether the localStorage data is stale
 
 // global cache
 var fn_cache_ls_available = null;
