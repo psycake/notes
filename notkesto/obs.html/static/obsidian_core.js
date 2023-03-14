a93ba53226c9c1923a2a8dfed8dca258afcc945a
psycake <52770775+psycake@users.noreply.github.com>
2023-03-13 20:03:03 -0600

    Add relative clause notes

--- notkesto/obs.html/static/obsidian_core.js
+++ notkesto/obs.html/static/obsidian_core.js
@@ -10,7 +10,7 @@ var CONFIGURED_HTML_URL_PREFIX = "/notkesto";
 var RELATIVE_PATHS = 0;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '134926701943417258567133424472086613864'                       // used to check whether the localStorage data is stale
+var gzip_hash = '89656765936901579123244815983738865001'                       // used to check whether the localStorage data is stale
 
 // global cache
 var fn_cache_ls_available = null;
